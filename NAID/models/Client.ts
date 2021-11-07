e95ecda45ddeb3e4f87bc9816d602d410a45ac80
Finley Sherwood <me@itsf.in>
2021-11-07 12:13:29 +1000

    Generate photos from specified properties

--- NAID/models/Client.ts
+++ NAID/models/Client.ts
@@ -2,31 +2,130 @@
 import fetch from 'node-fetch'
 import FormData from 'form-data'
 import qs from 'qs'
+import fs from 'fs'
+import tar from 'tar'
+
+type BBox = [number, number, number, number]
+type Time = [number, number, number]
+type Resolution = [number, number]
 
 interface QueryData {
     request: string
     evalscript: string
 }
 
+interface ResponseData {
+    json: Buffer
+    png: Buffer
+    unix: number
+}
+
 export default class Client {
     private access_token?: string
 
-    public async query(endpoint: string, data: QueryData) {
+    public async generatePhoto(bbox: BBox, time: Time, resolution: Resolution): Promise<ResponseData> {
+        function formatDate(year: number, month: number, day: number) {
+            let monthString = month < 10 ? `0${month}` : month
+            let dayString = day < 10 ? `0${day}` : day
+            return `${year}-${monthString}-${dayString}T00:00:00Z`
+        }
+        
+        let buffer = await this.query('process', {
+            request: `
+                {
+                    "input": {
+                        "bounds": {
+                            "bbox": ${JSON.stringify(bbox)}
+                        },
+                        "data": [
+                            {
+                                "type": "sentinel-2-l2a",
+                                "dataFilter": {
+                                    "timeRange": {
+                                        "from": "${formatDate(time[0], time[1], time[2])}",
+                                        "to": "${formatDate(time[0], time[1], time[2] + 1)}"
+                                    }
+                                }
+                            }
+                        ]
+                    },
+                    "output": {
+                        "width": ${resolution[0]},
+                        "height": ${resolution[1]},
+                        "responses": [{
+                            "identifier": "default",
+                            "format": {"type": "image/png"}
+                        }, {
+                            "identifier": "userdata",
+                            "format": {"type": "application/json"}
+                        }]
+                    }
+                }
+            `,
+            evalscript: `
+                function setup() {
+                    return {
+                        input: ["B02", "B03", "B04"],
+                        mosaicking: Mosaicking.ORBIT,
+                        output: { id:"default", bands: 3}
+                    }
+                }
+              
+                function updateOutputMetadata(scenes, inputMetadata, outputMetadata) {
+                    outputMetadata.userData = { "scenes":  scenes.orbits }
+                }
+              
+                function evaluatePixel(samples) {
+                    return [ 2.5*samples[0].B04, 2.5*samples[0].B03, 2.5*samples[0].B02 ]
+                }
+            `
+        })
+
+        let unix = Date.now()
+
+        fs.writeFileSync(`./output/tar/${unix}.tar`, buffer)
+
+        return new Promise((resolve, reject) => {
+            let i = 0
+            let json: Buffer
+            let png: Buffer
+
+            tar.t({file: `./output/tar/${unix}.tar`, onentry: entry => {
+                entry.on('data', (buffer: Buffer) => {
+                    /// @ts-ignore
+                    if (entry.path === 'userdata.json') {
+                        json = buffer
+                    /// @ts-ignore
+                    } else if (entry.path === 'default.png') {
+                        png = buffer
+                    }
+
+                    if (++i == 2)
+                        resolve({json, png, unix})
+                })
+            }})
+        })
+    }
+
+    private async query(endpoint: string, data: QueryData) {
         if (!this.access_token) throw new Error('Client is not logged in!')
 
         let formData = new FormData()
         formData.append('request', data.request)
         formData.append('evalscript', data.evalscript)
 
-        return await fetch(`https://services.sentinel-hub.com/api/v1/${endpoint}`, {
+        let res: ArrayBuffer = await fetch(`https://services.sentinel-hub.com/api/v1/${endpoint}`, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${this.access_token}`,
                 'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
-                'Content-Length': formData.getLength(() => {})
+                'Content-Length': formData.getLength(() => {}),
+                'Accept': 'application/tar'
             },
             body: formData
-        })//.then((res: any) => res.json())
+        }).then((res: Response) => res.arrayBuffer())
+
+        return Buffer.from(res)
     }
 
     public async login() {
--- NAID/models/Runtime.ts
+++ NAID/models/Runtime.ts
@@ -10,60 +10,18 @@ export default class Runtime {
         const client = new Client()
         await client.login()
 
-        let res = await client.query('process', {
-            request: `
-                {
-                    "input": {
-                        "bounds": {
-                            "properties": {
-                                "crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
-                            },
-                            "bbox": [
-                                13.822174072265625,
-                                45.85080395917834,
-                                14.55963134765625,
-                                46.29191774991382
-                            ]
-                        },
-                        "data": [
-                            {
-                                "type": "sentinel-2-l2a",
-                                "dataFilter": {
-                                    "timeRange": {
-                                        "from": "2018-10-01T00:00:00Z",
-                                        "to": "2018-12-31T00:00:00Z"
-                                    }
-                                }
-                            }
-                        ]
-                    },
-                    "output": {
-                        "width": 512,
-                        "height": 512
-                    }
-                }
-            `,
-            evalscript: `
-                //VERSION=3
+        const photo = await client.generatePhoto(
+            [145.683948, -16.783875, 145.698514, -16.773644], // Coordinates to photograph
+            [2020, 11, 14], // Photograph date
+            [512, 512] // Resolution of the image
+        )
 
-                function setup() {
-                    return {
-                        input: ["B02", "B03", "B04"],
-                        output: { 
-                        bands: 3, 
-                        sampleType: "AUTO" // default value - scales the output values from [0,1] to [0,255].
-                        }
-                    }
-                }
+        const {json, png, unix} = photo
 
-                function evaluatePixel(sample) {
-                    return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02]
-                }
-                `
-        })
-
-        console.log(res)
-
-        //fs.writeFileSync(`./output/${Date.now()}.png`, res)
+        const cloudCoverage = JSON.parse(json.toString()).scenes[0].tiles[0].cloudCoverage
+        const file = `./output/${unix} (${cloudCoverage}%).png`
+        
+        fs.createWriteStream(file).write(png)
+        console.log(`Successfully saved the file to ${file}`)
     }
 }
